2551a2d23f2da59c95820cff19b221e6640edf96
Abdul Aziz <abdulaziz@nusa.net.id>
2023-04-25 12:33:00 +0700

    update  backsound

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ window.addEventListener("scroll", function () {
 
   if (offset > 100) {
     document.getElementById("handler-top").classList.add("active");
+    document.getElementById("handler-audio").classList.add("active");
   } else {
     document.getElementById("handler-top").classList.remove("active");
+    document.getElementById("handler-audio").classList.remove("active");
   }
 });
 
@@ -19,11 +21,11 @@ window.addEventListener("scroll", function () {
   parallax1.style.backgroundPositionY = offset * 0.1 + "px";
 });
 
-function myFunction() {
+const myFunction = () => {
   document.getElementById("check").checked = false;
-}
+};
 
-function scrollInto(id) {
+const scrollInto = (id) => {
   let element = document.getElementById(id);
   var offsetPosition =
     element.getBoundingClientRect().top + window.pageYOffset - 120;
@@ -32,9 +34,9 @@ function scrollInto(id) {
     top: offsetPosition,
     behavior: "smooth",
   });
-}
+};
 
-function reveal() {
+const reveal = () => {
   var reveals = document.querySelectorAll(".reveal");
 
   for (var i = 0; i < reveals.length; i++) {
@@ -48,10 +50,63 @@ function reveal() {
       reveals[i].classList.remove("active");
     }
   }
-}
+};
 
 window.addEventListener("scroll", reveal);
 
 const topPage = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
+
+let isPlay = true;
+const muteAudio = () => {
+  var audioElem = document.getElementById("musicplayer");
+  isPlay = !isPlay;
+
+  if (isPlay) {
+    audioElem.muted = true;
+    audioElem.pause();
+  } else {
+    audioElem.muted = false;
+    audioElem.play();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.scrollTo(0, 0);
+});
+
+const openInvitation = () => {
+  document.body.classList.add("scrollable");
+};
+
+window.addEventListener("click", function () {
+  var audioElem = document.getElementById("musicplayer");
+  audioElem.play();
+});
+
+var d = new Date("2023-05-06 09:00:00");
+simplyCountdown(".simply-countdown-one", {
+  year: d.getFullYear(),
+  month: d.getMonth() + 1,
+  day: d.getDate(),
+});
+
+const params = new window.URLSearchParams(window.location.search);
+const receiver = params.get("to");
+if (receiver) {
+  let elementReceiver = document.getElementById("receiver");
+  let wrapperItem = document.createElement("div");
+  wrapperItem.className = "main-font fs-28 mb-2";
+  wrapperItem.setAttribute("style", "font-family: 'Quicksand', sans-serif;");
+  const itemText = document.createTextNode("Kepada: ");
+  wrapperItem.appendChild(itemText);
+
+  let receiverItem = document.createElement("div");
+  const receiverName = document.createTextNode(receiver);
+  receiverItem.appendChild(receiverName);
+  receiverItem.setAttribute("style", "font-family: 'Quicksand', sans-serif;");
+
+  elementReceiver.appendChild(wrapperItem);
+  elementReceiver.appendChild(receiverItem);
+}
